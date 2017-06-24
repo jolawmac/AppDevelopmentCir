311849322c1ba6c8a42eaec66c9f24969e2d848e
Joshua McDonald <joshua_mcdonald@me.com>
2017-06-24 18:29:00 -0500

    Completed chapter 16

--- 16_QuestionBot2/ChatBot/ChatBot/ConversationDataSource.swift
+++ 16_QuestionBot2/ChatBot/ChatBot/ConversationDataSource.swift
@@ -2,18 +2,25 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    var messageCount = 0
+    var messageCount: Int {
+        return messages.count
+    }
+    var messages = [openingLine]
     
     /// Add a new question to the conversation
     func add(question: String) {
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
         print("Asked to add question: \(question)")
-        messageCount += 1
+        //messageCount += 1
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
         print("Asked to add answer: \(answer)")
-        messageCount += 1
+        //messageCount += 1
     }
     
     /// The Message at a specific point in the conversation
@@ -21,11 +28,12 @@ class ConversationDataSource {
         print("Asking for message at index \(index)")
         //return Message(date: Date(), text: "Hello, world!", type: .answer)
         
-        if index % 2 == 0 {
-            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
-        } else {
-            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
-        }
+//        if index % 2 == 0 {
+//            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
+//        } else {
+//            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
+//        }
+        return messages[index]
         
     }
     
