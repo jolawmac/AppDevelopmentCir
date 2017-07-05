67db549792724b7b0c1cf403011bb392552ef1e4
Joshua McDonald <joshua_mcdonald@me.com>
2017-07-05 08:56:43 -0500

    Added story board and randomizer function to sign file

--- RPS2/Sign.swift
+++ RPS2/Sign.swift
@@ -9,7 +9,19 @@
 import Foundation
 import GameplayKit
 
-//let randomChoice =
+func randomSign() -> Sign {
+    let sign = randomChoice.nextInt()
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
+
+let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2)
 
 
 enum Sign {
