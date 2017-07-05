fa6961aa6814c755e9f98f46c02942af582042c1
Joshua McDonald <joshua_mcdonald@me.com>
2017-07-05 18:07:25 -0500

    Updated RPS app

--- RPS2/RPS2/ViewController.swift
+++ RPS2/RPS2/ViewController.swift
@@ -12,14 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var appSign: UILabel!
+    @IBOutlet weak var gameStatus: UILabel!
+    @IBOutlet weak var rock: UIButton!
+    @IBOutlet weak var paper: UIButton!
+    @IBOutlet weak var scissors: UIButton!
+    @IBOutlet weak var playAgainButtonTapped: UIButton!
 
 
+    @IBAction func playRock(_ sender: Any) {
+    }
+    
+    @IBAction func playPaper(_ sender: Any) {
+    }
+    
+    @IBAction func playScissors(_ sender: Any) {
+    }
+    
+    @IBAction func playAgainTapped(_ sender: Any) {
+    }
+    
 }
 
